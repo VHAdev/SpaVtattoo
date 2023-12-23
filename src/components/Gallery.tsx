6e94f161623fcb81bb406ee8f93e96d47772e50c
Vitor Hugo Ayres <vitorayres1997@hotmail.com>
2023-12-23 20:11:23 -0300

    unsplash imgs

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -2,16 +2,16 @@ import PhotoAlbum from "react-photo-album";
 
 const photos = [
 
-  { src: "assets/38c5c.jpg", width: 900, height: 1200},
-  { src: "assets/38c5c.jpg", width: 300, height: 500},
-  { src: "assets/38c5c.jpg", width: 700, height: 700},
-  { src: "assets/38c5c.jpg", width: 900, height: 1200},
-  { src: "assets/38c5c.jpg", width: 600, height: 600},
-  { src: "assets/38c5c.jpg", width: 500, height: 1000},
-  { src: "assets/38c5c.jpg", width: 300, height: 600},
-  { src: "assets/38c5c.jpg", width: 300, height: 500},
-  { src: "assets/38c5c.jpg", width: 300, height: 500},
-  { src: "assets/38c5c.jpg", width: 600, height: 800},
+  { src: "https://source.unsplash.com/random/?tattoo/1", width: 900, height: 1200},
+  { src: "https://source.unsplash.com/random/?tattoo/2", width: 300, height: 500},
+  { src: "https://source.unsplash.com/random/?tattoo/3", width: 700, height: 700},
+  { src: "https://source.unsplash.com/random/?tattoo/4", width: 900, height: 1200},
+  { src: "https://source.unsplash.com/random/?tattoo/5", width: 600, height: 600},
+  { src: "https://source.unsplash.com/random/?tattoo/6", width: 500, height: 1000},
+  { src: "https://source.unsplash.com/random/?tattoo/7", width: 300, height: 600},
+  { src: "https://source.unsplash.com/random/?tattoo/8", width: 300, height: 500},
+  { src: "https://source.unsplash.com/random/?tattoo/9", width: 300, height: 500},
+  { src: "https://source.unsplash.com/random/?tattoo/10", width: 600, height: 800},
 
 ];
 
